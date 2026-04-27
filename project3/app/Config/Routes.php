e967f35e8142bf5a1d6510b2fa5d9d3c66ee63ed
irchamali <id.irchamali@gmail.com>
2026-04-27 14:13:37 +0700

    Add login and registration using MythAUth

--- project3/app/Config/Routes.php
+++ project3/app/Config/Routes.php
@@ -13,8 +13,8 @@ $routes->get('/faqs', 'Page::faqs');
 $routes->get('/post', 'Post::index');
 $routes->get('/post/(:any)', 'Post::viewPost/$1');
 
-$routes->group('admin', function($routes){
-	$routes->get('post', 'PostAdmin::index'); // ['filter' => 'login']
+$routes->group('admin', ['filter' => 'login'],function($routes){ # update
+	$routes->get('post', 'PostAdmin::index'); 
 	$routes->get('post/(:segment)/preview', 'PostAdmin::preview/$1');
 	$routes->add('post/new', 'PostAdmin::create');
 	$routes->add('post/(:segment)/edit', 'PostAdmin::edit/$1');
